117f6bbafb5bbf39a9842af6c954010769098e38
Евгений <foxspb2012@gmail.com>
2022-06-16 15:52:10 +0300

    Выполнено задание "Обновление века(часть 2)".

--- src/utils/event.js
+++ src/utils/event.js
@@ -4,12 +4,13 @@ const formatDate = (date, format) => dayjs(date).format(format);
 
 const getDuration = (dateStart, dateEnd) => {
 
-  const difference = dayjs(dateEnd).diff(dayjs(dateStart), 'minute');
-  const day = Math.floor(difference / 1440).toString();
-  const hour = Math.floor(difference / 60).toString();
-  const minute = (difference % 60).toString();
+  const day = dayjs(dateEnd).diff(dayjs(dateStart), 'day');
+  const hour = dayjs(dateEnd).diff(dayjs(dateStart), 'hour') - day * 24;
+  const minute = dayjs(dateEnd).diff(dayjs(dateStart), 'minute') - day * 1440 - hour * 60;
 
-  return `${(day > 0 ? `${day < 10 ? `0${day}` : day}D `: '') + (hour > 0 ? `${hour < 10 ? `0${hour}`: hour }H `: '')  }${minute < 10 ? `0${minute}` : minute}M`;
+  return `${(day > 0 ? `${day < 10 ? `0${day}` : day}D `: '') +
+            (hour > 0 ? `${hour < 10 ? `0${hour}`: hour }H `: '') +
+            (minute > 0 ? `${minute < 10 ? `0${minute}`: minute }M `: '')}`;
 };
 
 const isDayExpired = (dueDate) => dueDate && dayjs().isAfter(dueDate, 'D');
